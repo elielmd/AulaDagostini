15e3dda02933b37dbd70ef97e289fe57d346f9a1
EMDOURADO <eliel.dourado27@gmail.com>
2016-03-16 21:05:47 -0300

    Versão 1.0!

---
+++ AulaGit/src/br/univel/Principal.java
@@ -0,0 +1,16 @@
+package br.univel;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		new Message().hello();
+		System.out.println("É nóis!");
+
+		System.out.println("Hello World");
+		System.out.println("Are we!");
+
+		
+	}
+	
+}
